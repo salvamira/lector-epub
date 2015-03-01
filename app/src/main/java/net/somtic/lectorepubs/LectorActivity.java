fc93ae66aa0815a51d28427d70f878fc7687191e
Salva Mira <smira@iesperemaria.com>
2015-03-01 13:33:17 +0100

    Commit inicial amb contributors

---
+++ app/src/main/java/net/somtic/lectorepubs/LectorActivity.java
@@ -0,0 +1,12 @@
+package net.somtic.lectorepubs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LectorActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
