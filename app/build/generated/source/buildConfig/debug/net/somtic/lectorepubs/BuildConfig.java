28f5167fa323634f77e67a39a0051e89484957a8
Salva Mira <smira@iesperemaria.com>
2015-03-01 18:43:30 +0100

    Commit inicial

--- app/build/generated/source/buildConfig/debug/net/somtic/lectorepubs/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/net/somtic/lectorepubs/BuildConfig.java
@@ -5,7 +5,7 @@ package net.somtic.lectorepubs;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.commonsware.empublite";
+  public static final String APPLICATION_ID = "net.somtic.lectorepubs";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
